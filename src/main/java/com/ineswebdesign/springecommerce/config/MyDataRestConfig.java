b5045e77a2849cd75ad6bf99915d1d44152a123f
Inés SEHILI <you@example.com>
2024-01-29 10:05:37 +0100

    disable HTTP methods for Product/ProductCategory : PUT , POST and DELETE

--- src/main/java/com/ineswebdesign/springecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/ineswebdesign/springecommerce/config/MyDataRestConfig.java
@@ -1,12 +1,24 @@
 package com.ineswebdesign.springecommerce.config;
 
+import com.ineswebdesign.springecommerce.entity.Product;
+import com.ineswebdesign.springecommerce.entity.ProductCategory;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+        HttpMethod[] theeSupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        //disable HTTP methods for Product : PUT , POST and DELETE
+        config.getExposureConfiguration().forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods)-> httpMethods.disable(theeSupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theeSupportedActions));
+        //disable HTTP methods for ProductCategory  : PUT , POST and DELETE
+        config.getExposureConfiguration().forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods)-> httpMethods.disable(theeSupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theeSupportedActions));
     }
 }
